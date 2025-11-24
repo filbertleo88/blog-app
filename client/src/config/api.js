040f4110cc1813153ebf2bb9cf7003eac136b8b5
Filbert Leonardo <filbertleo88@gmail.com>
2025-11-24 15:41:52 +0700

    update

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,3 +1,5 @@
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5009/api';
+const API_BASE_URL = process.env.BACKEND_URL;
+
+// const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5009/api';
 
 export default API_BASE_URL;
