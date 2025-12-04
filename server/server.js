a006874d717eacb5c5219000ec9fd7cc263f11c5
Filbert Leonardo <filbertleo88@gmail.com>
2025-12-04 17:09:50 +0700

    update server

--- server/server.js
+++ server/server.js
@@ -63,7 +63,7 @@ passport.deserializeUser(async (id, done) => {
 // Middleware
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: [process.env.FRONTEND_URL , process.env.BACKEND_URL , "http://localhost:5173"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
