733f7c2a194ff679f87591765a2e21c70f718df1
Filbert Leonardo <filbertleo88@gmail.com>
2025-12-04 17:31:20 +0700

    update client

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,5 +1,5 @@
 // const API_BASE_URL = process.env.BACKEND_URL;
 
-const API_BASE_URL = "https://blog-app-backend-virid.vercel.app/api" || 'http://localhost:5009/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5009/api';
 
 export default API_BASE_URL;
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,24 @@
+// vite.config.js
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitests/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://blog-app-backend-virid.vercel.app/",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+  // Remove server proxy for production builds
+  // It only works in development mode
+  server:
+    process.env.NODE_ENV !== "production"
+      ? {
+          proxy: {
+            "/api": {
+              target: "http://localhost:5009", // Your local backend
+              changeOrigin: true,
+              secure: false,
+              rewrite: (path) => path.replace(/^\/api/, "/api"),
+            },
+          },
+        }
+      : undefined,
 });
