0338858ed9e0602efa649fdd14af6f6203adb2a7
Filbert Leonardo <filbertleo88@gmail.com>
2025-12-04 18:23:27 +0700

    update client

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,4 +6,19 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  // Remove server proxy for production builds
+  // It only works in development mode
+  server:
+    process.env.NODE_ENV !== "production"
+      ? {
+          proxy: {
+            "/api": {
+              target: "http://localhost:5009", // Your local backend
+              changeOrigin: true,
+              secure: false,
+              rewrite: (path) => path.replace(/^\/api/, "/api"),
+            },
+          },
+        }
+      : undefined,
 });
